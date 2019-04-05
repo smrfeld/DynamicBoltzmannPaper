bb5dccff4f6e18e8163fb6d5f7d40c467af02f8d
Oliver K. Ernst <oernst@ucsd.edu>
2019-04-04 17:45:49 -0700

    visible file for bimol_train

--- bimol_train/src/main_visible.cpp
+++ bimol_train/src/main_visible.cpp
@@ -77,7 +77,7 @@ int main() {
 	cout << "--- Making RHS diff eq ---" << endl;
 
 	// Domain
-    double spacing = 0.4;
+    double spacing = 0.25; // 0.4
     double center = 0.0;
 	Domain1D* domain_1d_hA = new Domain1D(ixn_hA,spacing,center);
 	Domain1D* domain_1d_jAA = new Domain1D(ixn_jAA,spacing,center);
@@ -95,6 +95,17 @@ int main() {
 	auto rhs_kAAA = make_shared<DiffEqRHS>("DE kAAA", ixn_kAAA, domain, dopt);
 	ixn_kAAA->set_diff_eq_rhs(rhs_kAAA);
 
+    // Dependency
+    ixn_hA->add_diff_eq_dependency(ixn_hA->get_diff_eq_rhs(), 0);
+    ixn_hA->add_diff_eq_dependency(ixn_jAA->get_diff_eq_rhs(), 0);
+    ixn_hA->add_diff_eq_dependency(ixn_kAAA->get_diff_eq_rhs(), 0);
+    ixn_jAA->add_diff_eq_dependency(ixn_hA->get_diff_eq_rhs(), 1);
+    ixn_jAA->add_diff_eq_dependency(ixn_jAA->get_diff_eq_rhs(), 1);
+    ixn_jAA->add_diff_eq_dependency(ixn_kAAA->get_diff_eq_rhs(), 1);
+    ixn_kAAA->add_diff_eq_dependency(ixn_hA->get_diff_eq_rhs(), 2);
+    ixn_kAAA->add_diff_eq_dependency(ixn_jAA->get_diff_eq_rhs(), 2);
+    ixn_kAAA->add_diff_eq_dependency(ixn_kAAA->get_diff_eq_rhs(), 2);
+    
 	cout << "--- [Finished] Making RHS diff eq ---" << endl;
 	cout << endl;
 
@@ -123,34 +134,20 @@ int main() {
 	cout << "--- Making lattice ---" << endl;
 
 	// 1000 cube
-    auto latt = make_shared<Lattice>(1,1000,sp_vis,LatticeMode::NORMAL);
+    bool conn_nn = true;
+    bool conn_triplet = true;
+    auto latt = make_shared<LatticeTraj1DFullyVisible>(1000,sp_vis,conn_nn,conn_triplet);
 
 	cout << " > begin visible" << endl;
 
-	// NN, NNN
-	latt->all_conns_vv_init();
-	latt->all_conns_vvv_init();
-
-	// Set possible species
-	latt->all_units_v_add_possible_species(species_A);
-
-	// Make bias dict
-	auto bias_dict_visible = make_shared<BiasDict>();
-	bias_dict_visible->add_ixn(species_A,ixn_hA);
-	auto o2_dict_visible = make_shared<O2IxnDict>();
-	o2_dict_visible->add_ixn(species_A,species_A,ixn_jAA);
-	auto o3_dict_visible = make_shared<O3IxnDict>();
-	o3_dict_visible->add_ixn(species_A,species_A,species_A,ixn_kAAA);
-
-	// Add dict to latt
-	latt->all_units_v_set_bias_dict(bias_dict_visible);
-	latt->all_conns_vv_set_ixn_dict(o2_dict_visible);
-	latt->all_conns_vvv_set_ixn_dict(o3_dict_visible);
-
-	// Add all units to moment of ixn func
-	latt->all_units_v_add_to_moment_h(ixn_hA->get_moment());
-	latt->all_conns_vv_add_to_moment_j(ixn_jAA->get_moment());
-	latt->all_conns_vvv_add_to_moment_k(ixn_kAAA->get_moment());
+    // Set bias
+    latt->set_bias(species_A, ixn_hA);
+    
+    // NNs
+    latt->set_ixn_2(species_A, species_A, ixn_jAA);
+    
+    // Triplets
+    latt->set_ixn_3(species_A, species_A, species_A, ixn_kAAA);
 
 	cout << " > end visible" << endl;
 
@@ -159,94 +156,90 @@ int main() {
 	****************************************/
 
 	// Opt solver class
-	OptProblem opt(latt);
-
-	// Add ixn params
-	opt.add_ixn_param(ixn_hA);
-	opt.add_ixn_param(ixn_kAAA);
-	opt.add_ixn_param(ixn_jAA);
+	OptProblemDynamic opt(latt);
 
 	// Params
-	int no_opt_steps = 200-10;
-	// int no_timesteps = 100;
+	int no_opt_steps = 190;
+    
 	double dt=0.01;
-	int no_latt_sampling_steps = 1;
+	int no_cd_steps = 1;
+    
+    // Batch size
 	int batch_size = 10;
-	double dopt=1.0;
-
-	// Structures
-	// opt.init_structures(no_timesteps,batch_size);
+    latt->set_no_markov_chains(MCType::AWAKE, batch_size);
+    latt->set_no_markov_chains(MCType::ASLEEP, batch_size);
 
 	// Options
-	OptionsSolve options;
-	options.VERBOSE_NU = false;
-	options.VERBOSE_WAKE_ASLEEP = false;
-	options.VERBOSE_MOMENT = false;
-	options.MODE_random_integral_range = true;
-	options.VAL_random_integral_range_size = 10;
+	OptionsSolveDynamic options;
+    options.solver = Solver::SGD;
 
+    // Options wake sleep
+    OptionsWakeSleep_1DFV_CD options_wake_sleep;
+
+    // Dir to write to
+    std::string dir_write = "../data_learned_visible";
+
+    // No timesteps for ixn params
+    int no_timesteps = 200;
+    ixn_hA->set_no_timesteps(no_timesteps);
+    ixn_jAA->set_no_timesteps(no_timesteps);
+    ixn_kAAA->set_no_timesteps(no_timesteps);
+
+    // No timesteps
+    int timepoint_start_latt = 0;
+    int no_timesteps_latt = 5;
+    latt->set_no_timesteps(timepoint_start_latt, no_timesteps_latt);
+    
 	/********************
 	Read init cond
 	********************/
 
-	// clock_t start = clock();    
-
 	std::map<int,double> ixn_hA_IC, ixn_jAA_IC, ixn_kAAA_IC;
 	for (auto IC=1; IC<=50; IC++) {
-		auto init_cond_vector = read_init_conds("../bimol_train_stoch_sim/ic_v"+pad_str(IC,3)+"/init_visible.txt");
+		auto init_cond_vector = read_init_conds("../../bimol_train_stoch_sim/data/ic_v"+pad_str(IC,3)+"/init_visible.txt");
 
 		ixn_hA_IC[IC] = init_cond_vector[0];
 		ixn_jAA_IC[IC] = init_cond_vector[1];
 		ixn_kAAA_IC[IC] = init_cond_vector[2];
 	};
 
-	// clock_t stop = clock();    
-	// std::cout << (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC << std::endl;
-
 	/********************
 	Filenames
 	********************/
 
-	int no_timesteps_max = 100;
+	int no_timesteps_max = 200;
 
-	std::map<int, FNameSeriesColl> fnames_map;
-	for (auto IC=1; IC<=50; IC++) {
-		fnames_map[IC] = FNameSeriesColl();
-
-		for (auto i_batch=1; i_batch<=100; i_batch++) {
-
-			// Series of filenames
-			FNameSeries fname_series;
-			for (auto timepoint=0; timepoint<=no_timesteps_max; timepoint++) {
-				fname_series.fnames.push_back("../bimol_train_stoch_sim/ic_v"+pad_str(IC,3)+"/lattice_v"+pad_str(i_batch,3)+"/lattice/"+pad_str(timepoint,4)+".txt");
-			};
-
-			// Add
-			fnames_map[IC].add_fname_series(fname_series);
-		};
-	};
+    std::map<int, FNameTrajColl> fnames_map;
+    for (auto IC=1; IC<=50; IC++) {
+        fnames_map[IC] = FNameTrajColl();
+        
+        for (auto i_batch=1; i_batch<=50; i_batch++) {
+            
+            // Series of filenames
+            FNameTraj fname_traj;
+            for (auto timepoint=0; timepoint<=no_timesteps_max; timepoint++) {
+                fname_traj.push_back(FName("../../bimol_train_stoch_sim/data/ic_v"+pad_str(IC,3)+"/lattice_v"+pad_str(i_batch,3)+"/lattice/"+pad_str(timepoint,4)+".txt",true));
+            };
+            
+            // Add
+            fnames_map[IC].add_fname_traj(fname_traj);
+        };
+    };
 
 	/********************
 	Iterate over opt steps
 	********************/
 
-	// int IC = randI(1,20); // inclusive
-	int no_timesteps = 5;
-	options.VAL_random_integral_range_start = -5;
-	options.VAL_random_integral_range_end = -5;
+    int timepoint_start_solve_ixn_params = 0;
+    int no_timesteps_solve_ixns_params = 5;
 
-	// Opt step
 	for (auto opt_step=1; opt_step<=no_opt_steps; opt_step++) {
 
 		// Gradually increase window
-		if (opt_step % 10 == 1) {
-			no_timesteps += 5;
-			opt.init_structures(no_timesteps,batch_size);
-
-			// Endpt
-			// options.VAL_random_integral_range_end = no_timesteps - options.VAL_random_integral_range_size;
-			options.VAL_random_integral_range_start += 5;
-			options.VAL_random_integral_range_end += 5;
+        if (opt_step % 2 == 1 && opt_step != 1) {
+            timepoint_start_latt += 1;
+            no_timesteps_solve_ixns_params += 1;
+            latt->set_no_timesteps(timepoint_start_latt, no_timesteps_latt);
 		};
 
 		// All ICS
@@ -266,33 +259,20 @@ int main() {
 			********************/
 
 			// Take a step
-			opt.solve_one_step(opt_step,no_timesteps,batch_size,dt,dopt,no_latt_sampling_steps,fnames_map[IC],options);
+            opt.solve_one_step_1d_fully_visible(opt_step, timepoint_start_solve_ixn_params, no_timesteps_solve_ixns_params, timepoint_start_latt, no_timesteps_latt, timepoint_start_latt, no_timesteps_latt, dt, no_cd_steps, fnames_map.at(IC), options, options_wake_sleep);
 
-			// Sample moments (whole trajectory)
-			if (opt_step==no_opt_steps) {
-				opt.wake_sleep_loop(0,no_timesteps,batch_size,no_latt_sampling_steps,fnames_map[IC],false);
-			};
-
-			// Print moments
-			std::cout << ixn_hA->get_name() << " " << std::flush;
-			ixn_hA->get_moment()->print_moment_comparison();
-
-			std::cout << ixn_jAA->get_name() << " " << std::flush;
-			ixn_jAA->get_moment()->print_moment_comparison();
-
-			std::cout << ixn_kAAA->get_name() << " " << std::flush;
-			ixn_kAAA->get_moment()->print_moment_comparison();
-
-			// Write moments
-			ixn_hA->get_moment()->write_to_file("data_learned_visible/moments/"+ixn_hA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-			ixn_jAA->get_moment()->write_to_file("data_learned_visible/moments/"+ixn_jAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-			ixn_kAAA->get_moment()->write_to_file("data_learned_visible/moments/"+ixn_kAAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-
-			// Write ixn params
-			ixn_hA->write_to_file("data_learned_visible/ixn_params/"+ixn_hA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-			ixn_jAA->write_to_file("data_learned_visible/ixn_params/"+ixn_jAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-			ixn_kAAA->write_to_file("data_learned_visible/ixn_params/"+ixn_kAAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
-
+            // Write moments
+            /*
+            ixn_hA->write_moment_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/moments/"+ixn_hA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+            ixn_jAA->write_moment_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/moments/"+ixn_jAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+            ixn_kAAA->write_moment_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/moments/"+ixn_kAAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+            
+            // Write ixn params
+            ixn_hA->write_val_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/ixn_params/"+ixn_hA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+            ixn_jAA->write_val_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/ixn_params/"+ixn_jAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+            ixn_kAAA->write_val_traj_to_file(timepoint_start_latt, no_timesteps_latt, dir_write+"/ixn_params/"+ixn_kAAA->get_name()+"_"+pad_str(IC,3)+"_"+pad_str(opt_step,4)+".txt");
+             */
+            
 			// Write adjoint
 			/*
 			ixn_hA->get_adjoint()->write_to_file("data_learned_visible/adjoint/"+ixn_hA->get_name()+"_"+pad_str(opt_step,4)+".txt");
@@ -302,12 +282,12 @@ int main() {
 
 		};
 
-		// Write diff eq rhs
-		if (opt_step == no_opt_steps) {
-			rhs_hA->write_to_file("data_learned_visible/diff_eq_rhs/"+ixn_hA->get_name()+"_"+pad_str(opt_step,4)+".txt");
-			rhs_jAA->write_to_file("data_learned_visible/diff_eq_rhs/"+ixn_jAA->get_name()+"_"+pad_str(opt_step,4)+".txt");
-			rhs_kAAA->write_to_file("data_learned_visible/diff_eq_rhs/"+ixn_kAAA->get_name()+"_"+pad_str(opt_step,4)+".txt");
-		};
+        // Write diff eq rhs
+        if (opt_step == no_opt_steps) {
+            rhs_hA->write_to_file(dir_write+"/diff_eq_rhs/"+ixn_hA->get_name()+"_"+pad_str(opt_step,4)+".txt");
+            rhs_jAA->write_to_file(dir_write+"/diff_eq_rhs/"+ixn_jAA->get_name()+"_"+pad_str(opt_step,4)+".txt");
+            rhs_kAAA->write_to_file(dir_write+"/diff_eq_rhs/"+ixn_kAAA->get_name()+"_"+pad_str(opt_step,4)+".txt");
+        };
 	};
 
 	return 0;
